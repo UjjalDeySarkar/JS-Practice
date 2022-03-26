cf6a59c6f11590c9581fa2cf983637806b934b18
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-03-26 22:23:03 +0530

    create object in an object

--- object.js
+++ object.js
@@ -6,5 +6,13 @@ let obj = {}
 let cap = {
     fname : "Ujjal",
     lname : "Dey",
-    friends : ['Ankit', 'Abhishek', 'Anmol'] // create array in object
+    friends : ['Ankit', 'Abhishek', 'Anmol'], // create array in object
+    isInsperians : true,
+    age : 25,
+    //create object
+    address :{
+        state : "West Bengal",
+        city : "Siliguri"
+    }
+    
 }
