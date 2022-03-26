6dd9b888094518a5e0e44a22a56287e31628495a
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-03-27 02:29:27 +0530

    create test file and call it from another file

--- Node Modules/cp.js
+++ Node Modules/cp.js
@@ -9,8 +9,12 @@ const cp = require('child_process')
 // cp.execSync('code') 
 // console.log('VS code oppened')
 
-// open -a "Google Chrome"
-console.log('Trying to open chrome')
-// cp.execSync('start chrome') // for windows
-cp.execSync('open -a "Google Chrome" https://www.youtube.com/') // for mac
-console.log('chrome oppened')
+// // open -a "Google Chrome"
+// console.log('Trying to open chrome')
+// // cp.execSync('start chrome') // for windows
+// cp.execSync('open -a "Google Chrome" https://www.youtube.com/') // for mac
+// console.log('chrome oppened')
+
+let output = cp.execSync('node test.js')
+// console.log(output) // output will show in a buffer(binary format) form
+console.log("output is "+output) 
