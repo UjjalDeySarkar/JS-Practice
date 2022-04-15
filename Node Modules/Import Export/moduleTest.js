b3d4901ced6d329f3860b8ace95bfccf5c44f8d8
UjjalDeySarkar <ujjaldeysarkar123@gmail.com>
2022-04-15 10:12:31 +0530

    import and export module

---
+++ Node Modules/Import Export/moduleTest.js
@@ -0,0 +1,7 @@
+const calculatorModule = require('./calculator') // import module
+
+//use your imported methods
+calculatorModule.addition(2,3)
+calculatorModule.subtruction(3,2)
+calculatorModule.multiplication(3,2)
+calculatorModule.division(10,2)
---
+++ Node Modules/path.js
@@ -0,0 +1,9 @@
+const path = require('path')
+let ext = path.extname('//Users//ujjaldey//jsPractice//JS-Practice//Node Modules//f1.txt')
+console.log('Extention of file is -> ', ext)
+
+let basename = path.basename('//Users//ujjaldey//jsPractice//JS-Practice//Node Modules//f1.txt')
+console.log('basename of file is -> ', basename)
+
+console.log(__dirname)
+console.log(__filename)
